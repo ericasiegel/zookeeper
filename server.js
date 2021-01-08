90eaeafa0355a94c9816f1b947171c213efb42a7
Erica Siegel <siegel.erica@gmail.com>
2021-01-07 23:15:25 -0800

    added front end functionality

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// makes all public file folders available for the server's use
+app.use(express.static('public'));
 
 
 function filterByQuery(query, animalsArray) {
@@ -112,6 +114,18 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
